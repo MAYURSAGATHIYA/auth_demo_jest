bcd70d269eeb7952c5de9f27bb868867f9df2f29
Mayur <mayursagathiya98@gmail.com>
2022-02-28 15:51:44 +0530

    succesfullyvalidation testing done

--- test/regis.test.js
+++ test/regis.test.js
@@ -45,3 +45,44 @@
 //=====================================
 //registration testing
 //=====================================
+const supertest = require("supertest");
+
+const app = require("./app");
+
+
+const request = supertest.agent(app.callback())
+
+describe("registrattion checking", () => {
+    describe('middleware testing', () => {
+        var _isEmailindb = [{
+            first: "ok",
+            last: "ok2",
+            email: "123@yahoo.com",
+            password: "123123",
+            confirmpassword: "123123"
+        }]
+
+        test("all fields entered or not", async () => {
+            _isEmailindb = jest.fn(() => ({
+                status: true
+            }))
+
+            const data = await request.post("/registration").send({
+                first: "ok",
+        
+                email: "123@yahoo.com",
+                password: "1",
+                confirmpassword: "123123"
+            })
+            expect(data.body).toEqual({ "fields":  [
+                     "",
+                     "last",
+                     "",
+                     "",
+                     "",
+                   ],
+                   "message": "Validation Failed. Please enter required fields",
+                   "msg": "true",})
+        })
+    })
+})
