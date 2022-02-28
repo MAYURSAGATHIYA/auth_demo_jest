325fd9b14d0301a519fbfeb3c7683238bd45e722
Mayur <mayursagathiya98@gmail.com>
2022-02-28 16:57:31 +0530

    fixed bug in cases

--- test/regis.test.js
+++ test/regis.test.js
@@ -1,47 +1,3 @@
-
-// const supertest = require('supertest')
-// const app = require('./app')
-// // const authdata = require("./check_in_db/auth_check");
-// const authdata=require("../api/login_api")
-// const { captureRejectionSymbol } = require('koa')
-// // const jest=require('jest')
-
-// // const same_email = require('../middleware_for_reg_same')
-
-// const request = supertest.agent(app.callback())
-
-
-
-
-
-// describe("login testing",()=>{
-//     describe('email testing',() => {
-//         const _isEmailindb=[{email:"ciudbbdbuboboo@yahoo.com"}]
-//     console.log(_isEmailindb,"email i db avtr")
-        
-//         test("password entered or not", async () => {
-//             _isEmailindb=jest.fn(()=>({
-//                 status: true
-//             }))
-    
-//             const data=await request.post("/login").send({
-//                 email:"ciudbbdbuboboo@yahoo.com",
-//                 password:"111111"
-//             })
-//             expect(data.body).toEqual({ status: false, message: "Please Enter password" })
-//         })
-//         test("if email is not entered or the email is undefined",async()=>{
-//             const data=await request.post("/login").send({})
-//             expect(data.body).toEqual({ status: false, message: 'Please Enter email' })
-//         })
-//     })
-// })
-
-
-
-
-
-
 //=====================================
 //registration testing
 //=====================================
@@ -52,14 +8,13 @@ const app = require("./app");
 
 const request = supertest.agent(app.callback())
 
-describe("registrattion checking", () => {
+describe.skip("registrattion checking", () => {
     describe('middleware testing', () => {
         var _isEmailindb = [{
             first: "ok",
-            last: "ok2",
             email: "123@yahoo.com",
-            password: "123123",
-            confirmpassword: "123123"
+            password: "1111",
+            confirmpassword: "1111"
         }]
 
         test("all fields entered or not", async () => {
@@ -71,8 +26,8 @@ describe("registrattion checking", () => {
                 first: "ok",
         
                 email: "123@yahoo.com",
-                password: "1",
-                confirmpassword: "123123"
+                password: "1111",
+                confirmpassword: "1111"
             })
             expect(data.body).toEqual({ "fields":  [
                      "",
@@ -84,5 +39,78 @@ describe("registrattion checking", () => {
                    "message": "Validation Failed. Please enter required fields",
                    "msg": "true",})
         })
+
+
+
+        
     })
 })
+// =====================================================
+// same password
+// ===================================================
+describe("registrattion checking", () => {
+    describe('middleware testing', () => {
+        var _isEmailindb = [{
+            first: "ok",
+            last: "ok2",
+            email: "123@yahoo.com",
+            password: "1111",
+            confirmpassword: "1111"
+        }]
+
+        test(" checking password and confirm password similarity", async () => {
+            _isEmailindb = jest.fn(() => ({
+                status: true
+            }))
+
+            const data = await request.post("/registration").send({
+                first: "ok",
+                last: "qw",
+                email: "123@yahoo.com",
+                password: "1111",
+                confirmpassword: "1"
+            })
+            expect(data.body).toEqual({
+                message: "password should be the same"
+            })
+        })
+    })
+})
+
+//====================================================
+//same email id check
+//====================================================
+
+// const email_testing=require('../middleware_for_reg_same')
+// const same_email_testing=email_testing.emailcheck
+
+
+describe.skip("registrattion checking", () => {
+    describe('middleware testing', () => {
+        var _isEmailindb = [{
+            first: "ok",
+            last: "ok2",
+            email: "123@yahoo.com",
+            password: "1",
+            confirmpassword: "1"
+        }]
+
+        test("all fields entered or not", async () => {
+            _isEmailindb = jest.fn(() => ({
+                status: true
+            }))
+
+            const data = await request.post("/registration").send({
+                first: "ok",
+                last: "qw",
+                email: "123@yahoo.com",
+                password: "1",
+                confirmpassword: "1"
+            })
+            expect(data.body).toEqual({
+                message: "this email is already taken"
+            })
+        })
+    })
+})
+
